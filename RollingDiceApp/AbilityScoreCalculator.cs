efe380e333c2103d58ab3fac446a941c4c49f486
Mykola Kuzyk <ukrboy99@gmail.com>
2021-04-09 22:33:42 +0200

    Add project files.

---
+++ RollingDiceApp/AbilityScoreCalculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RollingDiceApp
+{
+    class AbilityScoreCalculator
+    {
+        public int RollResults = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score = 0;
+
+        public void CalculateAbilityScore()
+        {
+            int addAmount = AddAmount;
+            double divided = (double)RollResults / DivideBy;
+            int added = addAmount += (int)divided;
+
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            } else
+            {
+                Score = added;
+            }
+
+
+        }
+
+
+        // <summay>
+        // Writes a promt and reads an int value from console
+        // </summary>
+        // <param name="lastUsedValue"> The default value.</param>
+        // <param name = "prompt" > Promt to print to the console</param>
+        // <retusrn>The int value read, or default valueifunable to parse</returns> 
+
+        public static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.WriteLine(prompt + "["+ lastUsedValue+"]: ");
+            string line = Console.ReadLine();
+
+            if ( !int.TryParse(line, out int value))
+            {
+                Console.WriteLine("Unable to Parse using default value instead:" + lastUsedValue);
+                return lastUsedValue;
+            }
+            else
+            {
+                Console.WriteLine("Using Value:" + value);
+                return value;
+            }
+           
+
+
+        }
+
+         public static double ReadDouble(double lastUsedValue, string prompt) 
+        {
+            Console.WriteLine(prompt + "[" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+
+            if (!double.TryParse(line, out double value))
+            {
+                Console.WriteLine("Unable to Parse using default value instead:" + lastUsedValue);
+                return lastUsedValue;
+            }
+            else
+            {
+                Console.WriteLine("Using Value:" + value);
+                return value;
+            }
+
+        }
+
+    }
+}
---
+++ RollingDiceApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RollingDiceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            Console.WriteLine("Start");
+
+            while (true)
+            {
+                calculator.RollResults = AbilityScoreCalculator.ReadInt(calculator.RollResults, "Starting 4d6 roll");
+                calculator.DivideBy = AbilityScoreCalculator.ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = AbilityScoreCalculator.ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = AbilityScoreCalculator.ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Calculated ability score: " + calculator.Score);
+                Console.WriteLine("Press Q to quit, any other key to continiue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+
+            }
+        }
+    }
+}
